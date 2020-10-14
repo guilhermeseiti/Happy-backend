67ccb6c848109d3f008e6497ff0aae0291cec65a
Guilherme Seiti <gseitihomma@gmail.com>
2020-10-13 21:25:40 -0300

    added new column

--- src/database/migrations/1602633166107-create_orphanages.ts
+++ src/database/migrations/1602633166107-create_orphanages.ts
@@ -33,6 +33,9 @@ export class createOrphanages1602633166107 implements MigrationInterface {
                     name: 'instructions',
                     type: 'text',
                 },{
+                    name: 'opening_hours',
+                    type: 'varchar',
+                },{
                     name: 'open_on_weekends',
                     type: 'boolean',
                     default: false
