7ae1cebe7a809f0b696a5c4ced3d32e5d8290348
Guilherme Seiti <gseitihomma@gmail.com>
2020-10-14 06:50:41 -0300

    created find orphanage

--- src/controllers/OrphanagesController.ts
+++ src/controllers/OrphanagesController.ts
@@ -1,12 +1,21 @@
-import {Request, Response} from 'express';
-import {getRepository} from 'typeorm';
+import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
 import Orphanage from '../models/Orphanage';
 
 export default {
-    async index(request: Request, response: Response){
+    async index(request: Request, response: Response) {
         const orphanagesRepository = getRepository(Orphanage);
 
         const orphanages = await orphanagesRepository.find();
+
+        return response.json(orphanages);
+    },
+
+    async show(request: Request, response: Response) {
+        const { id } = request.params;
+        const orphanagesRepository = getRepository(Orphanage);
+
+        const orphanages = await orphanagesRepository.findOneOrFail();
 
         return response.json(orphanages);
     },
@@ -22,9 +31,9 @@ export default {
             opening_hours,
             open_on_weekends,
         } = request.body;
-    
+
         const orphanagesRepository = getRepository(Orphanage);
-    
+
         const orphanage = orphanagesRepository.create({
             id,
             name,
@@ -35,9 +44,9 @@ export default {
             opening_hours,
             open_on_weekends,
         });
-    
+
         await orphanagesRepository.save(orphanage);
-        
-        return  response.status(201).json(orphanage);
+
+        return response.status(201).json(orphanage);
     }
 }
